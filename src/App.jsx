e347acf08a212257da17049e4fb09da1b3613f1f
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-25 10:44:11 -0800

    wrote first fetch function using async/await

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { getPinBalls } from './services/promise-me';
+
 export default function App() {
+  console.log(getPinBalls());
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,6 +1,16 @@
 /**
  * TODO:
+ * i will be writiting two function, one with async/await and one with a .then()
+ * https://ponball.herokuapp.com/api/v1/machines
  */
+
+export const getPinBalls = async () => {
+  const response = await fetch('https://ponball.herokuapp.com/api/v1/machines');
+
+  const data = await response.json();
+
+  return data;
+};
 
 /**
  * TODO:
